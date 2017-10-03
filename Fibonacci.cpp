3d33450c8b18491bbb93efbf6292a87ed74cd63f
IceyRomance <353465987@qq.com>
2017-10-03 12:07:09 +0800

    #366 Fibonacci

--- Fibonacci.cpp
+++ Fibonacci.cpp
@@ -6,11 +6,11 @@ public:
      */
     int fibonacci(int n) {
         // write your code here
-    if (n <= 1) {
-		return n;
+    if (n <= 2) {
+		return n-1;
 	}
 	int n0 = 0, n1 = 1, n2 = 1;
-	for (int i = 2; i<n; ++i)
+	for (int i = 3; i<n; ++i)
 	{
 		n0 = n1;
 		n1 = n2;
